87d90ccbc167051117bd4440141311aeb72a9fe9
Niraj Dhandhukiya <nirajprajapati2330@gmail.com>
2024-04-05 15:37:57 +0530

    add corc

--- app.js
+++ app.js
@@ -26,9 +26,7 @@ app.use(express.urlencoded({extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/',((req,res)=>{
-  send.message("inddexfile")
-}));
+app.use('/', indexRouter);
    
 
 // error handler
